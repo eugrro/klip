3420f02b7cf498542db189e1fb8b0836a16758a6
eugroo <eugene.rozental@gmail.com>
2020-10-24 19:11:09 -0400

    adding server

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Klips',
-      home: Login(),
+      home: Navigation(),
     );
   }
 }
